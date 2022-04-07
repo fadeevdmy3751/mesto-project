e06b21e9afa04db68f777db5ebaa169e4cd76e71
dmitriy3751 <fdmitrij2009@yandex.ru>
2022-04-07 22:31:27 +0300

    third sprint second try

---
+++ scripts/initialCards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Мурманск',
+    link: 'https://images.unsplash.com/photo-1612860640446-3023ebe28e85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80'
+  },
+  {
+    name: 'Ак-кем',
+    link: 'https://www.vsedostoprimechatelnosti.ru/assets/cache/images/evropa/rossiya5/altajskij-kraj/akkemskoe-ozero-860x-2e7.jpg'
+  },
+  {
+    name: 'Рускеала',
+    link: 'https://images.unsplash.com/photo-1548288242-d454d4648b55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80'
+  },
+  {
+    name: 'Чарские пески',
+    link: 'https://cdn.fishki.net/upload/post/2020/05/30/3331186/464032765c4dfd02da79b370f92da681.jpg'
+  },
+  {
+    name: 'Териберка',
+    link: 'https://images.unsplash.com/photo-1624719635330-c03b1ed0c02a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+  },
+  {
+    name: 'Вулканы Камчатки',
+    link: 'https://s3.nat-geo.ru/images/2019/8/10/1da96e6d650c4161ba95fd23b8bc73a0.max-2500x1500.jpg'
+  },
+];
