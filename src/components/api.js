f7e083b4417bd330421fd7df75563b2262bfc335
dmitriy3751 <fdmitrij2009@yandex.ru>
2022-07-01 01:06:53 +0300

    oop started

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,40 @@
 import {TOKEN, COHORT} from './secret.js';
 import {refreshLikes} from "./card.js";
 
+export class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;   // url, по которому происходит обращение
+    this._headers = headers;   // headers, передаваемые в запросе по урлу
+  }
+
+  getInitialCards(){
+    return fetch(this._baseUrl + URLs.cards, {headers: this._headers})
+      .then(res => this._checkResponse(res, 'Ошибка getInitialCards: '))
+  }
+
+  // метод проверки ответа от сервера на наличие ошибки
+  // при её наличии - возврат Promise.reject
+  // если всё ок - возврат полученного сообщения в json
+  _checkResponse(res, errorMes) {
+    if (res.ok) {
+      return res.json();
+    }
+    // если ошибка, отклоняем промис
+    return Promise.reject(`${errorMes + res.status}`);
+  }
+
+}
+
+const api = new Api({
+  baseUrl: 'https://nomoreparties.co/v1/cohort-42',
+  headers: {
+    authorization: 'c56e30dc-2883-4270-a59e-b2f7bae969c6',
+    'Content-Type': 'application/json'
+  }
+});
+
+
+// из старого кода - конфиг хедера для подключения к серверу
 const config = {
   baseUrl: 'https://nomoreparties.co/v1/' + COHORT,
     headers: {
@@ -9,6 +43,7 @@ const config = {
     }
 }
 
+// из старого кода - окончания урлов для каждого из используемых методов
 const URLs = {
   cards: '/cards',
   me: '/users/me',
@@ -16,18 +51,18 @@ const URLs = {
   avatar: '/users/me/avatar'
 }
 
-function checkResponse(res, errorMes) {
-  if (res.ok) {
-    return res.json();
-  }
-  // если ошибка, отклоняем промис
-  return Promise.reject(`${errorMes + res.status}`);
-}
+// function checkResponse(res, errorMes) {
+//   if (res.ok) {
+//     return res.json();
+//   }
+//   // если ошибка, отклоняем промис
+//   return Promise.reject(`${errorMes + res.status}`);
+// }
 
-export function getInitialCards(){
-  return fetch(config.baseUrl + URLs.cards, {headers: config.headers})
-    .then(res => checkResponse(res, 'Ошибка getInitialCards: '))
-}
+// export function getInitialCards(){
+//   return fetch(config.baseUrl + URLs.cards, {headers: config.headers})
+//     .then(res => checkResponse(res, 'Ошибка getInitialCards: '))
+// }
 
 export function getMe(){
   return fetch(config.baseUrl + URLs.me,{headers: config.headers})
