4a1a154da1e9ae5fcdaf3f86ae5a88282598aa1e
fadeevdmy3751 <fadeevdmy7991@gmail.com>
2022-07-07 16:52:18 +0300

    добавлен jsdoc + seteventlisteners публичный

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,29 +1,49 @@
-
+/**
+ * Класс, который отвечает за открытие и закрытие попапа
+ */
 export default class Popup {
+  /**
+   * Конструктор класса
+   * @param selector - селектор попапа.
+   */
   constructor (selector) {
-    this._setEventListeners = this._setEventListeners.bind(this)
+    this.setEventListeners = this.setEventListeners.bind(this)
     this._handleEscClose = this._handleEscClose.bind(this);
     this.selector = selector
   };
 
+  /**
+   * Метод, отвечающий за открытие попапа
+   */
   open() {
     this.selector.classList.add('popup_opened');
     document.addEventListener('keyup', this._handleEscClose);
-    document.addEventListener('mousedown', this._setEventListeners);
+    document.addEventListener('mousedown', this.setEventListeners);
   };
 
+  /**
+   * Метод, отвечающий за закрытие попапа
+   */
   close() {
     this.selector.classList.remove('popup_opened');
     document.removeEventListener('keyup', this._handleEscClose);
-    document.removeEventListener('mousedown', this._setEventListeners);
+    document.removeEventListener('mousedown', this.setEventListeners);
   };
 
+  /**
+   * Метод, который содержит логику закрытия попапа клавишей Esc
+   * @param evt - event события в EventListener.
+   */
   _handleEscClose(evt){
     if (evt.key === "Escape")
       this.close();
   };
 
-  _setEventListeners(evt) {
+  /**
+   * Метод, который добавляет слушатель клика иконке закрытия попапа
+   * @param evt - event события в EventListener.
+   */
+  setEventListeners(evt) {
     if (evt.target.classList.contains('popup__close') || evt.target.classList.contains('popup'))
       this.close();
   };
